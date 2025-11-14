a5181bdc2dc0d968d8ca9cf46b078001262d2102
Nicko Bryan Davalos <bryandavalos06@gmail.com>
2025-11-14 13:45:22 +0800

    added some switch statements

--- app.java
+++ app.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class app {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         System.out.println("*8===================================================8*\r\n" +  
                         "        _____           .__                  .__   \r\n" + 
                         "       /  _  \\    ____  |__|  _____  _____   |  |\r\n" + 
@@ -19,12 +20,45 @@ public class app {
                         "\r\n" +
                         "                  by The Missionaries                 \r\n" +
                         "*8===================================================8*\n");
-        System.out.println("                        [1] Play\n");
-        System.out.println("                    [2] How to Play\n");
-        System.out.println("                        [3] Exit\n");
-        System.out.println("*8===================================================8*\n");
-        System.out.print("Enter Your Selection: ");
-        int opt = sc.nextInt();
+
+        int opt;
+
+        do {
+            // Show menu
+            System.out.println("                        [1] Play\n");
+            System.out.println("                    [2] How to Play\n");
+            System.out.println("                        [3] Exit\n");
+            System.out.println("*8===================================================8*\n");
+
+            System.out.print("Enter Your Selection: ");
+            opt = sc.nextInt();
+
+
+            switch (opt) {
+                case 1:
+                    play();
+                    break;
+                case 2:
+                    rules();
+                    break;
+                case 3:
+                    System.out.println("Thanks for playing!!");
+                    break;
+                default:
+                    System.err.println("Invalid Input. Please choose 1-3.");
+                    break;
+            }
+
+        } while (opt < 1 || opt > 3);
+
+        sc.close();
     }
-    
+
+    public static void play() {
+        System.out.println("Placeholder");
+    }
+
+    public static void rules() {
+        System.out.println("Placeholder2");
+    }
 }
